40f19f577845a7e069565c5e69d06bedb38cb7e6
Thomas Clark <snowmanchu@gmail.com>
2018-05-07 18:13:53 -0700

    Added kingpin to parse command line

--- main.go
+++ main.go
@@ -1,22 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"os"
+	kingpin "gopkg.in/alecthomas/kingpin.v2"
 )
 
 var apiURL = "http://google.ca"
 var exitStatus = 0
 
+var (
+	debug   = kingpin.Flag("debug", "Enable debug mode.").Bool()
+	timeout = kingpin.Flag("timeout", "Timeout waiting for ping.").Default("5s").OverrideDefaultFromEnvar("PING_TIMEOUT").Short('t').Duration()
+	ip      = kingpin.Arg("ip", "IP address to ping.").Required().IP()
+	count   = kingpin.Arg("count", "Number of packets to send").Int()
+)
+
 func main() {
-	argsWithProg := os.Args
-	argsWithoutProg := os.Args[1:]
+	kingpin.Version("0.0.1")
+	kingpin.Parse()
 
-	arg := os.Args[1]
-	fmt.Println(argsWithProg)
-	fmt.Println(argsWithoutProg)
-	fmt.Println(arg)
+	// argsWithProg := os.Args
+	// argsWithoutProg := os.Args[1:]
 
-	cr := commandRunner{command: arg, args: os.Args[2]}
-	cr.run()
+	// arg := os.Args[1]
+	// fmt.Println(argsWithProg)
+	// fmt.Println(argsWithoutProg)
+	// fmt.Println(arg)
+
+	// cr := commandRunner{command: arg, args: os.Args[2]}
+	// cr.run()
 }
